c4b698498066607d728db76edda85ea6613d17f1
hello <45997837+XuShengYuu@users.noreply.github.com>
2021-06-01 21:23:12 +0800

    fix bug

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -57,7 +57,7 @@ export default {
     ...mapMutations(['changeCity'])
   },
   mounted () {
-    this.scroll = new Bscroll(this.$refs.wrapper)
+    this.scroll = new Bscroll(this.$refs.wrapper, {click: true})
   },
   watch: {
     letter () {
